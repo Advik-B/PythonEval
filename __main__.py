1df7ed8e5ce199b9737f7b48d9a68a802eb3d5fb
Advik <advik.b@gmail.com>
2022-05-13 14:27:55 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_restful import Api, Resource
-import pkg_resources
-
+from flask_restful import Api
+from api import ListPyPiPackages
 
 # Define the basic config
 PORT = 2612
@@ -9,28 +8,19 @@ PORT = 2612
 
 # Create the application instance
 # __name__ is the name of the current module
-app = Flask(__name__, template_folder='html', static_folder='static', root_path='.')
+app = Flask(__name__, template_folder="html", static_folder="static", root_path=".")
 app_api = Api(app)
 
-class ListPyPi(Resource):
-    def _get(self):
-        packages = []
-        for package in pkg_resources.working_set:
-            packages.append({package.as_requirement(): package.version})
-        
-        return packages, 200
 
-    def get(self):
-        return self._get()
-        
-
-@app.route('/')
+@app.route("/")
 def startpage():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 def main():
-    app_api.add_resource(ListPyPi, '/api/packages')
+    app_api.add_resource(ListPyPiPackages, "/api/packages/list")
     app.run(debug=True, port=PORT)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
